7f42c379197cbf1200d25bf80248ebcdf2067f06
Joan <Joan@DESKTOP-D4KOPVH>
2024-10-18 01:28:39 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace employee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.ratePerHourTextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.salaryLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textjobtitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.departmenttextBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lastnametext = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Firstnametext = new System.Windows.Forms.TextBox();
+            this.Firstnamet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hoursWorkedTextBox
+            // 
+            this.hoursWorkedTextBox.Location = new System.Drawing.Point(501, 220);
+            this.hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+            this.hoursWorkedTextBox.Size = new System.Drawing.Size(137, 20);
+            this.hoursWorkedTextBox.TabIndex = 37;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(497, 197);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(164, 20);
+            this.label12.TabIndex = 36;
+            this.label12.Text = "Total Hours worked";
+            // 
+            // ratePerHourTextBox
+            // 
+            this.ratePerHourTextBox.Location = new System.Drawing.Point(149, 220);
+            this.ratePerHourTextBox.Name = "ratePerHourTextBox";
+            this.ratePerHourTextBox.Size = new System.Drawing.Size(137, 20);
+            this.ratePerHourTextBox.TabIndex = 35;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(145, 197);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(120, 20);
+            this.label11.TabIndex = 34;
+            this.label11.Text = "Rete per hour";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(145, 377);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 20);
+            this.label10.TabIndex = 33;
+            this.label10.Text = "Last name";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(145, 433);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 20);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "Basic Salary";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(290, 325);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(135, 20);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "First name here";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(290, 377);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 20);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "last name here";
+            // 
+            // salaryLabel
+            // 
+            this.salaryLabel.AutoSize = true;
+            this.salaryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaryLabel.Location = new System.Drawing.Point(290, 433);
+            this.salaryLabel.Name = "salaryLabel";
+            this.salaryLabel.Size = new System.Drawing.Size(19, 20);
+            this.salaryLabel.TabIndex = 29;
+            this.salaryLabel.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(145, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "First name";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(337, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 36);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Compute Salary";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textjobtitle
+            // 
+            this.textjobtitle.Location = new System.Drawing.Point(480, 103);
+            this.textjobtitle.Name = "textjobtitle";
+            this.textjobtitle.Size = new System.Drawing.Size(137, 20);
+            this.textjobtitle.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(476, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Job Title";
+            // 
+            // departmenttextBox2
+            // 
+            this.departmenttextBox2.Location = new System.Drawing.Point(144, 103);
+            this.departmenttextBox2.Name = "departmenttextBox2";
+            this.departmenttextBox2.Size = new System.Drawing.Size(137, 20);
+            this.departmenttextBox2.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(140, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Department";
+            // 
+            // Lastnametext
+            // 
+            this.Lastnametext.Location = new System.Drawing.Point(480, 20);
+            this.Lastnametext.Name = "Lastnametext";
+            this.Lastnametext.Size = new System.Drawing.Size(137, 20);
+            this.Lastnametext.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(476, -3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Last name";
+            // 
+            // Firstnametext
+            // 
+            this.Firstnametext.Location = new System.Drawing.Point(144, 20);
+            this.Firstnametext.Name = "Firstnametext";
+            this.Firstnametext.Size = new System.Drawing.Size(137, 20);
+            this.Firstnametext.TabIndex = 20;
+            // 
+            // Firstnamet
+            // 
+            this.Firstnamet.AutoSize = true;
+            this.Firstnamet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Firstnamet.Location = new System.Drawing.Point(140, -3);
+            this.Firstnamet.Name = "Firstnamet";
+            this.Firstnamet.Size = new System.Drawing.Size(94, 20);
+            this.Firstnamet.TabIndex = 19;
+            this.Firstnamet.Text = "First name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 456);
+            this.Controls.Add(this.hoursWorkedTextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.ratePerHourTextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.salaryLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textjobtitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.departmenttextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Lastnametext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Firstnametext);
+            this.Controls.Add(this.Firstnamet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hoursWorkedTextBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox ratePerHourTextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label salaryLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textjobtitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox departmenttextBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Lastnametext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Firstnametext;
+        private System.Windows.Forms.Label Firstnamet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace employee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        public class PartTimeEmployee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Department { get; set; }
+            public string JobTitle { get; set; }
+            public double BasicSalary { get; private set; }
+
+            public void ComputeSalary(int hoursWorked, double ratePerHour)
+            {
+                BasicSalary = hoursWorked * ratePerHour;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Firstnametext.Text) ||
+                       string.IsNullOrWhiteSpace(Lastnametext.Text) ||
+                       string.IsNullOrWhiteSpace(departmenttextBox2.Text) ||
+                       string.IsNullOrWhiteSpace(textjobtitle.Text) ||
+                       string.IsNullOrWhiteSpace(ratePerHourTextBox.Text) ||
+                       string.IsNullOrWhiteSpace(hoursWorkedTextBox.Text))
+            {
+                MessageBox.Show("Please fill in all ");
+                return;
+            }
+
+
+            if (double.TryParse(ratePerHourTextBox.Text, out double ratePerHour) &&
+            int.TryParse(hoursWorkedTextBox.Text, out int hoursWorked))
+            {
+                PartTimeEmployee employee = new PartTimeEmployee
+                {
+                    FirstName = Firstnametext.Text,
+                    LastName = Lastnametext.Text,
+                    Department = departmenttextBox2.Text,
+                    JobTitle = textjobtitle.Text
+                };
+
+
+                employee.ComputeSalary(hoursWorked, ratePerHour);
+
+                label8.Text = employee.FirstName; 
+                label7.Text = employee.LastName; 
+                salaryLabel.Text = employee.BasicSalary.ToString("");
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid number for rate per hour and hours worked");
+            }
+        }
+    }
+}
